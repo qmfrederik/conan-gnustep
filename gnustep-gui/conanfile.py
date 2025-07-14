a3d52178e4ed85ab598fa0ed67e26c2651bc2b41
Frederik Carlier <frederik.carlier@keysight.com>
2025-07-14 23:02:23 +0200

    Add helper method to configure Windows host

--- gnustep-gui/conanfile.py
+++ gnustep-gui/conanfile.py
@@ -111,19 +111,10 @@ class GnustepGuiRecipe(ConanFile):
         # in build failures.
         tc.configure_args.append("--srcdir=.")
 
-        if self.settings.os == "Windows":
-            # On Windows, force targetting native Windows, even when building in an MSYS2 shell (we're somewhat cross-compiling
-            # from MSYS2 to native Windows, even though we're using the _native_ tooling within MSYS2).
-            # If these variables are not set, gnustep-make will include -lm in the libs used when compiling, resulting in a
-            # compiler error when building gnustep-base.
-            #
-            # The output in configure logs for tools-make should be like this:
-            # checking build system type... x86_64-w64-mingw32
-            # checking host system type... x86_64-pc-windows
-            # checking target system type... x86_64-pc-windows
-            tc.configure_args.append(f"--host=x86_64-pc-windows")
-            tc.configure_args.append(f"--target=x86_64-pc-windows")
+        # On Windows, force targetting native Windows, even when building in an MSYS2 shell
+        self.python_requires["gnustep-helpers"].module.configure_windows_host(self, tc)
 
+        if self.settings.os == "Windows":
             # Generate pkg-config data for dependencies, which we can inject into the configure process.
             print(f"Generating pkg-config data in {self.generators_folder}")
             deps = PkgConfigDeps(self)
--- gnustep-helpers/conanfile.py
+++ gnustep-helpers/conanfile.py
@@ -4,6 +4,20 @@ import package_version
 def get_package_version(repository, package_name):
     return package_version.get_package_version(repository, package_name)
 
+def configure_windows_host(pkg, autotools):
+    if pkg.settings.os == "Windows":
+        # On Windows, force targetting native Windows, even when building in an MSYS2 shell (we're somewhat cross-compiling
+        # from MSYS2 to native Windows, even though we're using the _native_ tooling within MSYS2).
+        # If these variables are not set, gnustep-make will include -lm in the libs used when compiling, resulting in a
+        # compiler error when building gnustep-base.
+        #
+        # The output in configure logs for tools-make should be like this:
+        # checking build system type... x86_64-w64-mingw32
+        # checking host system type... x86_64-pc-windows
+        # checking target system type... x86_64-pc-windows
+        autotools.configure_args.append(f"--host=x86_64-pc-windows")
+        autotools.configure_args.append(f"--target=x86_64-pc-windows")
+
 class Pkg(ConanFile):
     name = "gnustep-helpers"
     version = "0.1"
