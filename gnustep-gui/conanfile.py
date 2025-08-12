f4f395ef68cb50f40a44d6f555d5a9f7f8654d28
Frederik Carlier <frederik.carlier@keysight.com>
2025-08-12 14:28:38 +0000

    gnustep-gui: Be flexible in with version ranges

--- gnustep-gui/conanfile.py
+++ gnustep-gui/conanfile.py
@@ -31,7 +31,7 @@ class GnustepGuiRecipe(ConanFile):
 
     def requirements(self):
         self.requires("gnustep-base/[^1.31.1]")
-        self.requires("libjpeg/9e")
+        self.requires("libjpeg/[>=9e]")
         self.requires("libtiff/4.7.0")
         self.requires("libpng/1.6.50")
         self.requires("giflib/5.2.2")
